dde4214744aab899cd55c455ca1ded04a532d4d1
yunjihan <sdkrer@gmail.com>
2012-04-22 23:29:47 +0900

    set up configuration!!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 indie-toolbox
 =============
+
+
+git clone git@github.com:yunjihan/indie-toolbox.git
+
+Making and executing script needs to install 'nokogiri' library on your computer.
+at least, you have ruby whose version is 1.8 or later.
+
+'gems install nokogiri' # you are getting it.
+
+sudo apt-get install libopenssl-ruby
+
+refer the following site about installing ruby through rvm
+http://ryanbigg.com/2010/12/ubuntu-ruby-rvm-rails-and-you/
--- tool/get-tweetbot.rb
+++ tool/get-tweetbot.rb
@@ -1,7 +1,20 @@
-#!/usr/local/bin/ruby
+#!/home/jihan/.rvm/bin/ruby
 # vi:set fileencoding=utf-8 :
 # indie bot...
 
-require 'open-uri'
+#require 'open-uri'
 require 'rubygems'
-require 'nokogiri'
+#require 'nokogiri'
+require 'tweetstream'
+
+puts "Tweetbot collector v0.1"
+puts TweetStream::VERSION
+
+TweetStream.configure do |config|
+  config.consumer_key = 'YuI89f43YzNR85uZvl6qyw'
+  config.consumer_secret = 'wWkWLghITcEDaah9YsM6xFuAdXrk7OY8X1ztAvgaKIc'
+  config.oauth_token = '104524073-NWFQMPHWu4KyIVwswdx7mKDmVWattxGB7Ie5M1un'
+  config.oauth_token_secret = 'NdI9T7E4eJOyX3F0Lu5vuw6OcR0TFtCjAlYje4ebEuE'
+  config.auth_method = :oauth
+  config.parser   = :yajl
+end
