fc9e87a4f0574a152eb77d2121ed1c3c003254bb
yunjihan <sdkrer@gmail.com>
2012-04-26 01:34:14 +0900

    parse data from muzever.com

--- tool/get-html.rb
+++ tool/get-html.rb
@@ -19,10 +19,41 @@ uri_json = URI.parse('http://muzever.com/api/events/get_by_date/?option=from_now
 doc_json = JSON.parse(uri_json.read)
 
 index=1
-doc_json.each do | x |
-	puts "#{index}. x.class: #{x.class}"
-	puts "x.inspect : #{x.inspect}"
+
+# for debugging
+#doc_json.each do | x |
+#	puts "#{index}. x.class: #{x.class}"
+#	puts "x.inspect : #{x.inspect}"
+#	index+=1
+#end
+
+
+puts doc_json.class # Hash
+# [0]
+status = doc_json["status"]
+
+# [2] 
+next_page = doc_json["next_page"];
+
+# [3]
+events	= doc_json["events"];
+
+# events_data = events[1];
+
+puts "events data"
+puts events.class
+
+index=1
+for event in events
+	# puts event.class # hash
+	# puts event.inspect
+	puts index.to_s + ": event!"
+	event.each do | key, value |
+		if !value.nil? 
+			puts "[" + key + " => " + value.to_s + "]"
+		end
+	end
+	puts ""
 	index+=1
 end
-
 # puts json_obj
