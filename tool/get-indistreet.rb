b7f397552bd93fa94653401f87c04d4b67040298
WeeyoungJeong <jwy1985@naver.com>
2012-04-30 00:28:22 +0900

    parse data from indistreet temporarily.
    Signed-off-by: WeeyoungJeong <jwy1985@naver.com>

--- tool/get-indistreet.rb
+++ tool/get-indistreet.rb
@@ -8,7 +8,24 @@ require "nokogiri"
 puts 'Indistreet parser v0.1'
 
 url = "http://indistreet.com/stage?page=1"
-doc = Nokogiri::HTML(doc = open(url))
-doc.css('div.entryStage').each do  | stage |
-  puts stage.content
+doc = Nokogiri::HTML(doc = open(url), 'UTF-8')
+
+doc.css('img.stageThumb').each do  | stage |
+	puts stage['src']
+end
+
+doc.css('a.title//h4').each do  | stage |
+	puts stage.content 
+end
+
+doc.css('a.stagePlaces').each do  | stage |
+	puts stage.content 
+end
+
+doc.css('p.stageDateTime//em.time').each do  | stage |
+	puts stage.content 
+end
+
+doc.css('ul.stageMusicians').each do  | stage |
+	puts stage.content 
 end
